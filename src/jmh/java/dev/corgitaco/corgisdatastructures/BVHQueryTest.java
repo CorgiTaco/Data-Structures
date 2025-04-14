a7554247b90de5d9b2abae4f83ef424886c029b3
CorgiTaco <66983020+CorgiTaco@users.noreply.github.com>
2025-04-14 04:03:45 -0700

    Test tweaks

--- src/jmh/java/dev/corgitaco/corgisdatastructures/BVHQueryTest.java
+++ src/jmh/java/dev/corgitaco/corgisdatastructures/BVHQueryTest.java
@@ -16,7 +16,7 @@ import java.util.Random;
 @BenchmarkMode(Mode.All)
 public class BVHQueryTest {
 
-    @Param({ "25", "50", "100", "250", "500", "1000", "2500", "5000", "10000", "25000", "50000", "100000", "250000", "500000", "1000000", "2500000", "5000000", "10000000", "25000000", "50000000" })
+    @Param({ "25", "50", "100", "250", "500", "1000", "2500", "5000", "10000", "25000", "50000", "100000", "250000", "500000", "1000000", "2500000", "5000000"})
     public int entries;
     BVH2D<Box> boxBVH2D = BVH2D.floodedBVH(42, entries, Integer.MAX_VALUE, 1, 5000000, true);
     Random random = new Random(42);
